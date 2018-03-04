69549186be0839d327e8846466fb90ca1762ce67
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-03-05 00:51:02 +0530

    Added new project

---
+++ LocationAlbum/LocationAlbum/CurrentLocationVC.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocationAlbum
+//
+//  Created by Salman Qureshi on 3/5/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
