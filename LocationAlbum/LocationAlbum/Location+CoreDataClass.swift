a006d42e3f6a2e5e8457d6d568e358bc3fd48060
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-03-13 03:24:53 +0530

    Adding Core data to the app

---
+++ LocationAlbum/LocationAlbum/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  LocationAlbum
+//
+//  Created by Salman Qureshi on 3/13/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ LocationAlbum/LocationAlbum/Location+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Location+CoreDataProperties.swift
+//  LocationAlbum
+//
+//  Created by Salman Qureshi on 3/13/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date
+    @NSManaged public var locationDescription: String
+    @NSManaged public var category: String
+    @NSManaged public var placemark: CLPlacemark?
+
+}
