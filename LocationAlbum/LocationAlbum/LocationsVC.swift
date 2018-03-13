ea6dc4c9527f8b8e4b2ac018822382e2fd3fbd12
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-03-14 03:59:54 +0530

    Bugfix in locations list

--- LocationAlbum/LocationAlbum/LocationsVC.swift
+++ LocationAlbum/LocationAlbum/LocationsVC.swift
@@ -113,61 +113,54 @@ class LocationsVC: UITableViewController {
 }
 
 extension LocationsVC: NSFetchedResultsControllerDelegate {
-    func controllerWillChangeContent(_ controller:
-        NSFetchedResultsController<NSFetchRequestResult>) {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         print("*** controllerWillChangeContent")
         tableView.beginUpdates()
     }
     
-    func controller(
-        _ controller: NSFetchedResultsController<NSFetchRequestResult>,
-        didChange anObject: Any, at indexPath: IndexPath?,
-        for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
         switch type {
         case .insert:
             print("*** NSFetchedResultsChangeInsert (object)")
             tableView.insertRows(at: [newIndexPath!], with: .fade)
+            
         case .delete:
             print("*** NSFetchedResultsChangeDelete (object)")
             tableView.deleteRows(at: [indexPath!], with: .fade)
+            
         case .update:
             print("*** NSFetchedResultsChangeUpdate (object)")
-            if let cell = tableView.cellForRow(at: indexPath!)
-                as? LocationCell {
+            if let cell = tableView.cellForRow(at: indexPath!) as? LocationCell {
                 let location = controller.object(at: indexPath!) as! Location
                 cell.configure(for: location)
             }
+            
         case .move:
             print("*** NSFetchedResultsChangeMove (object)")
             tableView.deleteRows(at: [indexPath!], with: .fade)
             tableView.insertRows(at: [newIndexPath!], with: .fade)
         }
-        
-        
-        func controller(
-            _ controller: NSFetchedResultsController<NSFetchRequestResult>,
-            didChange sectionInfo: NSFetchedResultsSectionInfo,
-            atSectionIndex sectionIndex: Int,
-            for type: NSFetchedResultsChangeType) {
-            switch type {
-            case .insert:
-                print("*** NSFetchedResultsChangeInsert (section)")
-                tableView.insertSections(IndexSet(integer: sectionIndex),
-                                         with: .fade)
-            case .delete:
-                print("*** NSFetchedResultsChangeDelete (section)")
-                tableView.deleteSections(IndexSet(integer: sectionIndex),
-                                         with: .fade)
-            case .update:
-                print("*** NSFetchedResultsChangeUpdate (section)")
-            case .move:
-                print("*** NSFetchedResultsChangeMove (section)")
-            }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            print("*** NSFetchedResultsChangeInsert (section)")
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            print("*** NSFetchedResultsChangeDelete (section)")
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .update:
+            print("*** NSFetchedResultsChangeUpdate (section)")
+        case .move:
+            print("*** NSFetchedResultsChangeMove (section)")
         }
-        func controllerDidChangeContent(
-            _ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-            print("*** controllerDidChangeContent")
-            tableView.endUpdates()
-        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("*** controllerDidChangeContent")
+        tableView.endUpdates()
     }
 }
