9b34671d7f1104f1505d859eda22312c13b24b30
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-03-14 01:41:22 +0530

    Design the prototype location cell

---
+++ LocationAlbum/LocationAlbum/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  LocationAlbum
+//
+//  Created by Salman Qureshi on 3/14/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
