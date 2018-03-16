754948aef82dab8be64e3519563fbb66ddb0bba3
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-03-17 04:43:48 +0530

    Make custom pins

--- LocationAlbum/LocationAlbum/MapVC.swift
+++ LocationAlbum/LocationAlbum/MapVC.swift
@@ -100,6 +100,8 @@ class MapVC: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    @objc func showLocationDetails(_ sender: UIButton) {
+    }
 
     /*
     // MARK: - Navigation
@@ -114,5 +116,42 @@ class MapVC: UIViewController {
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Location else {
+            return nil
+        }
+        
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView(
+            withIdentifier: identifier)
+        let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        
+        if annotationView == nil {
+            
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop = false
+            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82,
+            blue: 0.4, alpha: 1)
+            
+            let rightButton = UIButton(type: .detailDisclosure)
+            rightButton.addTarget(self,
+                                  action: #selector(showLocationDetails),
+                                  for: .touchUpInside)
+            pinView.rightCalloutAccessoryView = rightButton
+            
+            annotationView = pinView
+        }
+        
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            // 5
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            if let index = locations.index(of: annotation as! Location) {
+                button.tag = index
+            }
+        }
+        
+        return annotationView
+    }
 }
